a478d548fdbedb973e9f3c8c36d031b820a2ca7e
AdemirBurgic <ademir.burgic@gmail.com>
2015-06-12 01:38:49 +0200

    Must fix cout for DATE

---
+++ customer.cpp
@@ -0,0 +1,53 @@
+#include "customer.h"
+#include <string>
+#include <iostream>
+
+Customer::Customer(const Customer &b)
+{
+  _name = b._name;
+  _surname = b._surname;
+  _JMBG = b._JMBG;
+  _car = b._car;
+  _date = b._date;
+}
+
+Customer::Customer(Customer &&b)
+{
+  _name = b._name;
+  _surname = b._surname;
+  _JMBG = b._JMBG;
+  _car = b._car;
+  _date = b._date;
+}
+
+Customer& Customer::operator=(Customer&& b)
+{
+  _name = b._name;
+  _surname = b._surname;
+  _JMBG = b._JMBG;
+  _car = b._car;
+  _date = b._date;
+
+  return *this;
+}
+
+Customer& Customer::operator=(const Customer& b)
+{
+  _name = b._name;
+  _surname = b._surname;
+  _JMBG = b._JMBG;
+  _car = b._car;
+  _date = b._date;
+
+  return *this;
+}
+
+void Customer::print()
+{
+  std::cout << "Ime kupca: " << _name << std::endl;
+  std::cout << "Prezime kupca: " << _surname << std::endl;
+  std::cout << "JMBG kupca: " << _JMBG << std::endl;
+  std::cout << "Datum kupovine automobila: " << _date;
+  std::cout << "Informacije o kupljenom automobilu: " << std::endl;
+  _car.print();
+}
