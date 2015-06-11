5b0f614da22994f2d37bace9703c12aaa5732fd7
AdemirBurgic <ademir.burgic@gmail.com>
2015-06-12 01:32:00 +0200

    Added classes that describes date.

---
+++ date.h
@@ -0,0 +1,30 @@
+#ifndef _DATE_H
+#define _DATE_H
+
+#include <string>
+#include <iostream>
+
+class Date
+{
+  private:
+    std::string _day;
+    std::string _month;
+    std::string _year;
+
+  public:
+    Date() : _day(""), _month(""), _year("") {}
+    Date(const std::string& day, const std::string& month, const std::string& year) {}
+
+    Date(const Date&);
+    Date(Date&&);
+
+    Date& operator=(Date &&);
+    Date& operator=(const Date &);
+
+    friend ostream& operator<<(ostream& out, const Date& date)
+    {
+      out << date._day << "." << date._month << "." << date._year;
+      return out;
+    }
+};
+#endif
