5568ffb69711725c822d37c6cbd4cdb0defcf305
almir95 <almir94@live.com>
2015-06-19 12:19:40 +0200

    fixed error in worker.h

--- worker.h
+++ worker.h
@@ -6,15 +6,15 @@
 class Worker
 {
   private:
-    string _id_worker;
-    string _password;
+    std::string _id_worker;
+    std::string _password;
     std::string _name;
     std::string _surname;
 
   public:
     Worker() : _id_worker(""), _password(""), _name(""), _surname("") {}
 
-    Worker(const std::string& id_worker, const std::string& password, const string& name,const string& surname) : _id_worker(id_worker), _password(password), _name(name), _surname(surname) {}
+    Worker(const std::string& id_worker, const std::string& password, const std::string& name,const std::string& surname) : _id_worker(id_worker), _password(password), _name(name), _surname(surname) {}
 
     Worker(Worker&&);
     Worker(const Worker&);
